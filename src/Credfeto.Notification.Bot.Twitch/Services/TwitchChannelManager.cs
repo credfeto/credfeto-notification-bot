a0049bf934ca50f0e07ca87dd13628f5025d2837
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-02-27 16:15:14 +0000

    Added to constructor

--- src/Credfeto.Notification.Bot.Twitch/Services/TwitchChannelManager.cs
+++ src/Credfeto.Notification.Bot.Twitch/Services/TwitchChannelManager.cs
@@ -1,15 +1,22 @@
 using System;
 using Credfeto.Notification.Bot.Twitch.Models;
+using Microsoft.Extensions.Logging;
 using NonBlocking;
 
 namespace Credfeto.Notification.Bot.Twitch.Services;
 
 public sealed class TwitchChannelManager : ITwitchChannelManager
 {
+    private readonly ILogger<TwitchChannelManager> _logger;
+    private readonly IRaidWelcome _raidWelcome;
+    private readonly IShoutoutJoiner _shoutoutJoiner;
     private readonly ConcurrentDictionary<string, ChannelState> _streamStates;
 
-    public TwitchChannelManager()
+    public TwitchChannelManager(IRaidWelcome raidWelcome, IShoutoutJoiner shoutoutJoiner, ILogger<TwitchChannelManager> logger)
     {
+        this._raidWelcome = raidWelcome ?? throw new ArgumentNullException(nameof(raidWelcome));
+        this._shoutoutJoiner = shoutoutJoiner ?? throw new ArgumentNullException(nameof(shoutoutJoiner));
+        this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
         this._streamStates = new(comparer: StringComparer.OrdinalIgnoreCase);
     }
 
