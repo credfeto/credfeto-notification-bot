0d9fc11101a3ac243333b86a0f96ad52b2fd5c75
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-03-26 22:12:43 +0000

    Simplified test condition

--- src/Credfeto.Notification.Bot.Shared.Tests/Services/CurrentDateTimeSourceTests.cs
+++ src/Credfeto.Notification.Bot.Shared.Tests/Services/CurrentDateTimeSourceTests.cs
@@ -19,14 +19,12 @@ public sealed class CurrentDateTimeSourceTests : TestBase
     [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0002: Call Date Time abstraction", Justification = "This is implementing the referenced mechanism")]
     public void CurrentDateTimeSource_GetUtcNow_ReturnsCurrentDateTime()
     {
-        // Arrange
         DateTime expected = DateTime.UtcNow;
         ICurrentTimeSource currentDateTimeSource = this._currentDateTimeSource;
 
-        // Act
         DateTime result = currentDateTimeSource.UtcNow();
 
-        // Assert
-        Assert.Equal(expected: expected, actual: result);
+        TimeSpan difference = result - expected;
+        Assert.True(difference.TotalMilliseconds is >= 0 and < 100, $"Should be within a reasonable tolerance.  Currently ${difference.TotalMilliseconds}ms");
     }
 }
