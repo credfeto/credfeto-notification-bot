70d6a10056e15e66af39486b0cf3fc4e7ad79a82
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-04-08 22:30:19 +0100

    NO no messages but stuff happens

--- src/Credfeto.Notification.Bot.Twitch/Publishers/TwitchFollowerMilestoneReachedNotificationHandler.cs
+++ src/Credfeto.Notification.Bot.Twitch/Publishers/TwitchFollowerMilestoneReachedNotificationHandler.cs
@@ -1,26 +1,42 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Credfeto.Notification.Bot.Shared;
 using Credfeto.Notification.Bot.Twitch.Actions.Services;
+using Credfeto.Notification.Bot.Twitch.Configuration;
+using Credfeto.Notification.Bot.Twitch.Extensions;
 using Credfeto.Notification.Bot.Twitch.Models;
 using Credfeto.Notification.Bot.Twitch.StreamState;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Credfeto.Notification.Bot.Twitch.Publishers;
 
 public sealed class TwitchFollowerMilestoneReachedNotificationHandler : MessageSenderBase, INotificationHandler<TwitchFollowerMilestoneReached>
 {
     private readonly ILogger<TwitchFollowerMilestoneReachedNotificationHandler> _logger;
+    private readonly TwitchBotOptions _options;
 
-    public TwitchFollowerMilestoneReachedNotificationHandler(IMessageChannel<TwitchChatMessage> twitchChatMessageChannel, ILogger<TwitchFollowerMilestoneReachedNotificationHandler> logger)
+    public TwitchFollowerMilestoneReachedNotificationHandler(IOptions<TwitchBotOptions> options,
+                                                             IMessageChannel<TwitchChatMessage> twitchChatMessageChannel,
+                                                             ILogger<TwitchFollowerMilestoneReachedNotificationHandler> logger)
         : base(twitchChatMessageChannel)
     {
-        this._logger = logger;
+        this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        this._options = options.Value ?? throw new ArgumentNullException(nameof(options));
     }
 
     public async Task Handle(TwitchFollowerMilestoneReached notification, CancellationToken cancellationToken)
     {
+        TwitchModChannel? modChannel = this._options.GetModChannel(notification.Channel);
+
+        if (modChannel?.MileStones.Enabled != true)
+        {
+            return;
+        }
+
         await this.SendMessageAsync(channel: notification.Channel, $"/me @{notification.Channel} Woo! {notification.MilestoneReached} followers reached!", cancellationToken: cancellationToken);
 
         this._logger.LogWarning($"{notification.Channel}: Woo!! New follower milestone reached {notification.MilestoneReached}");
