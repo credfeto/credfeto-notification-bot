8b0fc436bac44d62f69c6c854e841dadb6f827f9
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-01 20:08:22 +0100

    Announce milestones using settings and overrides

--- src/Credfeto.Notification.Bot.Twitch/Publishers/TwitchFollowerMilestoneReachedNotificationHandler.cs
+++ src/Credfeto.Notification.Bot.Twitch/Publishers/TwitchFollowerMilestoneReachedNotificationHandler.cs
@@ -3,36 +3,33 @@ using System.Threading;
 using System.Threading.Tasks;
 using Credfeto.Notification.Bot.Shared;
 using Credfeto.Notification.Bot.Twitch.Actions.Services;
-using Credfeto.Notification.Bot.Twitch.Configuration;
-using Credfeto.Notification.Bot.Twitch.Extensions;
+using Credfeto.Notification.Bot.Twitch.Interfaces;
 using Credfeto.Notification.Bot.Twitch.Models;
 using Credfeto.Notification.Bot.Twitch.StreamState;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Credfeto.Notification.Bot.Twitch.Publishers;
 
 public sealed class TwitchFollowerMilestoneReachedNotificationHandler : MessageSenderBase, INotificationHandler<TwitchFollowerMilestoneReached>
 {
     private readonly ILogger<TwitchFollowerMilestoneReachedNotificationHandler> _logger;
-    private readonly TwitchBotOptions _options;
+    private readonly ITwitchChannelManager _twitchChannelManager;
 
-    public TwitchFollowerMilestoneReachedNotificationHandler(IOptions<TwitchBotOptions> options,
+    public TwitchFollowerMilestoneReachedNotificationHandler(ITwitchChannelManager twitchChannelManager,
                                                              IMessageChannel<TwitchChatMessage> twitchChatMessageChannel,
                                                              ILogger<TwitchFollowerMilestoneReachedNotificationHandler> logger)
         : base(twitchChatMessageChannel)
     {
+        this._twitchChannelManager = twitchChannelManager ?? throw new ArgumentNullException(nameof(twitchChannelManager));
         this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
-        this._options = options.Value ?? throw new ArgumentNullException(nameof(options));
     }
 
     public async Task Handle(TwitchFollowerMilestoneReached notification, CancellationToken cancellationToken)
     {
-        TwitchModChannel? modChannel = this._options.GetModChannel(notification.Streamer);
+        ITwitchChannelState channelState = this._twitchChannelManager.GetStreamer(notification.Streamer);
 
-        if (modChannel?.MileStones.Enabled != true)
+        if (channelState.Settings.AnnounceMilestonesEnabled)
         {
             return;
         }
