b648a2e14917dec2de35051e3f96805202d77563
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-25 23:10:09 +0100

    Additional tests

--- src/Credfeto.Notification.Bot.Twitch.Tests/Publishers/TwitchGiftSubMultipleNotificationHandlerTests.cs
+++ src/Credfeto.Notification.Bot.Twitch.Tests/Publishers/TwitchGiftSubMultipleNotificationHandlerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Credfeto.Notification.Bot.Twitch.Actions;
@@ -7,6 +8,7 @@ using Credfeto.Notification.Bot.Twitch.Publishers;
 using FunFair.Test.Common;
 using MediatR;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Xunit;
 
 namespace Credfeto.Notification.Bot.Twitch.Tests.Publishers;
@@ -22,13 +24,23 @@ public sealed class TwitchGiftSubMultipleNotificationHandlerTests : TestBase
     {
         this._contributionThanks = GetSubstitute<IContributionThanks>();
 
-        this._notificationHandler =
-            new TwitchGiftSubMultipleNotificationHandler(contributionThanks: this._contributionThanks, this.GetTypedLogger<TwitchGiftSubMultipleNotificationHandler>());
+        this._notificationHandler = new TwitchGiftSubMultipleNotificationHandler(contributionThanks: this._contributionThanks, this.GetTypedLogger<TwitchGiftSubMultipleNotificationHandler>());
     }
 
     [Fact]
     public async Task HandleAsync()
     {
+        await this._notificationHandler.Handle(new(streamer: Streamer, user: GiftedBy, count: 42), cancellationToken: CancellationToken.None);
+
+        await this.ThankForMultipleGiftSubsAsync();
+    }
+
+    [Fact]
+    public async Task HandleExceptionAsync()
+    {
+        this._contributionThanks.ThankForMultipleGiftSubsAsync(Arg.Any<Streamer>(), Arg.Any<Viewer>(), Arg.Any<int>(), Arg.Any<CancellationToken>())
+            .Throws<TimeoutException>();
+
         await this._notificationHandler.Handle(new(streamer: Streamer, user: GiftedBy, count: 42), cancellationToken: CancellationToken.None);
 
         await this.ThankForMultipleGiftSubsAsync();
