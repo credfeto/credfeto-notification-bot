a45d3c8e73c2e938fdfda3da217e6d40ca52d614
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-25 21:41:10 +0100

    Additional tests

--- src/Credfeto.Notification.Bot.Twitch.Tests/Publishers/TwitchStreamOnlineNotificationHandlerTests.cs
+++ src/Credfeto.Notification.Bot.Twitch.Tests/Publishers/TwitchStreamOnlineNotificationHandlerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Credfeto.Notification.Bot.Twitch.Configuration;
@@ -16,6 +17,7 @@ namespace Credfeto.Notification.Bot.Twitch.Tests.Publishers;
 public sealed class TwitchStreamOnlineNotificationHandlerTests : TestBase
 {
     private static readonly Streamer Streamer = Streamer.FromString(nameof(Streamer));
+    private static readonly Streamer NonStreamer = Streamer.FromString(nameof(NonStreamer));
 
     private readonly INotificationHandler<TwitchStreamOnline> _notificationHandler;
 
@@ -36,18 +38,32 @@ public sealed class TwitchStreamOnlineNotificationHandlerTests : TestBase
     }
 
     [Fact]
-    public async Task HandleAsync()
+    public async Task HandleWhenModChannelAsync()
     {
         this._twitchChannelManager.GetStreamer(Streamer)
             .Returns(this._twitchChannelState);
 
-        await this._notificationHandler.Handle(new(streamer: Streamer, title: "Skydiving", gameName: "IRL", new(year: 2020, month: 1, day: 1)),
-                                               cancellationToken: CancellationToken.None);
+        await this._notificationHandler.Handle(new(streamer: Streamer, title: "Skydiving", gameName: "IRL", new(year: 2020, month: 1, day: 1)), cancellationToken: CancellationToken.None);
 
         this._twitchChannelManager.Received(1)
             .GetStreamer(Streamer);
 
         await this._twitchChannelState.Received(1)
                   .OnlineAsync(gameName: "IRL", new(year: 2020, month: 1, day: 1));
+    }
+
+    [Fact]
+    public async Task HandleWhenNotModChannelAsync()
+    {
+        this._twitchChannelManager.GetStreamer(Arg.Any<Streamer>())
+            .Returns(this._twitchChannelState);
+
+        await this._notificationHandler.Handle(new(streamer: NonStreamer, title: "Skydiving", gameName: "IRL", new(year: 2020, month: 1, day: 1)), cancellationToken: CancellationToken.None);
+
+        this._twitchChannelManager.DidNotReceive()
+            .GetStreamer(Arg.Any<Streamer>());
+
+        await this._twitchChannelState.DidNotReceive()
+                  .OnlineAsync(Arg.Any<string>(), Arg.Any<DateTime>());
     }
 }
