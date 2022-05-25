c6a6810686e3e4a073b9227c7e674f4534a4c355
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-25 23:55:10 +0100

    Check that welcomes are not sent when they are turned off

--- src/Credfeto.Notification.Bot.Twitch.Tests/Actions/Services/WelcomeWaggonTests.cs
+++ src/Credfeto.Notification.Bot.Twitch.Tests/Actions/Services/WelcomeWaggonTests.cs
@@ -16,6 +16,7 @@ public sealed class WelcomeWaggonTests : TestBase
 {
     private static readonly Streamer Streamer = Streamer.FromString(nameof(Streamer));
     private static readonly Viewer User = Viewer.FromString(nameof(User));
+    private readonly ITwitchChannelState _twitchChannelState;
     private readonly IMessageChannel<TwitchChatMessage> _twitchChatMessageChannel;
     private readonly IWelcomeWaggon _welcomeWaggon;
 
@@ -25,27 +26,42 @@ public sealed class WelcomeWaggonTests : TestBase
 
         ITwitchChannelManager twitchChannelManager = GetSubstitute<ITwitchChannelManager>();
 
-        ITwitchChannelState channel = GetSubstitute<ITwitchChannelState>();
-        channel.Settings.ChatWelcomesEnabled.Returns(true);
+        this._twitchChannelState = GetSubstitute<ITwitchChannelState>();
         twitchChannelManager.GetStreamer(Streamer)
-                            .Returns(channel);
+                            .Returns(this._twitchChannelState);
 
-        this._welcomeWaggon = new WelcomeWaggon(twitchChannelManager: twitchChannelManager,
-                                                twitchChatMessageChannel: this._twitchChatMessageChannel,
-                                                this.GetTypedLogger<WelcomeWaggon>());
+        this._welcomeWaggon = new WelcomeWaggon(twitchChannelManager: twitchChannelManager, twitchChatMessageChannel: this._twitchChatMessageChannel, this.GetTypedLogger<WelcomeWaggon>());
     }
 
     [Fact]
     public async Task WelcomeAsync()
     {
+        this._twitchChannelState.Settings.ChatWelcomesEnabled.Returns(true);
+
         await this._welcomeWaggon.IssueWelcomeAsync(streamer: Streamer, user: User, cancellationToken: CancellationToken.None);
 
         await this.ReceivedPublishMessageAsync($"Hi @{User}");
+    }
+
+    [Fact]
+    public async Task NotWelcomeWhenWelcomesDisabledAsync()
+    {
+        this._twitchChannelState.Settings.ChatWelcomesEnabled.Returns(false);
+
+        await this._welcomeWaggon.IssueWelcomeAsync(streamer: Streamer, user: User, cancellationToken: CancellationToken.None);
+
+        await this.DidNotReceivePublishMessageAsync();
     }
 
     private ValueTask ReceivedPublishMessageAsync(string expectedMessage)
     {
         return this._twitchChatMessageChannel.Received(1)
                    .PublishAsync(Arg.Is<TwitchChatMessage>(t => t.Streamer == Streamer && t.Message == expectedMessage), Arg.Any<CancellationToken>());
+    }
+
+    private ValueTask DidNotReceivePublishMessageAsync()
+    {
+        return this._twitchChatMessageChannel.DidNotReceive()
+                   .PublishAsync(Arg.Any<TwitchChatMessage>(), Arg.Any<CancellationToken>());
     }
 }
