f1897c547d336dea9ecafdef09dfcd240a77ce2d
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-25 19:40:49 +0100

    Additional logging

--- src/Credfeto.Notification.Bot.Discord/BackgroundServices/DiscordTestServices.cs
+++ src/Credfeto.Notification.Bot.Discord/BackgroundServices/DiscordTestServices.cs
@@ -1,20 +1,25 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Credfeto.Notification.Bot.Discord.BackgroundServices;
 
 public sealed class DiscordTestServices : BackgroundService
 {
     private readonly IDiscordBot _discordBot;
+    private readonly ILogger<DiscordTestServices> _logger;
 
-    public DiscordTestServices(IDiscordBot discordBot)
+    public DiscordTestServices(IDiscordBot discordBot, ILogger<DiscordTestServices> logger)
     {
         this._discordBot = discordBot;
+        this._logger = logger;
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        this._logger.LogDebug($"Starting {this._discordBot.GetType().FullName}");
+
         return Task.CompletedTask;
     }
 }
