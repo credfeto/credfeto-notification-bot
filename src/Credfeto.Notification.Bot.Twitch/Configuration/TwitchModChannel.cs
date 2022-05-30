a27a83917b8de16e687ff5c04ea25da12beb6344
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-30 21:26:38 +0100

    Added special case for nulls

--- src/Credfeto.Notification.Bot.Twitch/Configuration/TwitchModChannel.cs
+++ src/Credfeto.Notification.Bot.Twitch/Configuration/TwitchModChannel.cs
@@ -5,14 +5,19 @@ namespace Credfeto.Notification.Bot.Twitch.Configuration;
 public sealed class TwitchModChannel
 {
     [JsonConstructor]
-    public TwitchModChannel(string channelName, TwitchChannelShoutout shoutOuts, TwitchChannelRaids raids, TwitchChannelThanks thanks, TwitchChannelMileStone mileStones, TwitchChannelWelcome welcome)
+    public TwitchModChannel(string channelName,
+                            TwitchChannelShoutout? shoutOuts,
+                            TwitchChannelRaids? raids,
+                            TwitchChannelThanks? thanks,
+                            TwitchChannelMileStone? mileStones,
+                            TwitchChannelWelcome? welcome)
     {
         this.ChannelName = channelName;
-        this.ShoutOuts = shoutOuts;
-        this.Raids = raids;
-        this.Thanks = thanks;
-        this.MileStones = mileStones;
-        this.Welcome = welcome;
+        this.ShoutOuts = shoutOuts ?? new TwitchChannelShoutout(enabled: false, new());
+        this.Raids = raids ?? new TwitchChannelRaids(enabled: false, immediate: null, calmDown: null);
+        this.Thanks = thanks ?? new TwitchChannelThanks(enabled: false);
+        this.MileStones = mileStones ?? new TwitchChannelMileStone(enabled: false);
+        this.Welcome = welcome ?? new TwitchChannelWelcome(false);
     }
 
     public string ChannelName { get; }
