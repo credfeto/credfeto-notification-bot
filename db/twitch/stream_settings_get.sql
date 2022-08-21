dbbb64523e54525956882ee904d181e6c046d827
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-08-21 20:53:16 +0100

    Added stream_settings_get procedure

---
+++ db/twitch/stream_settings_get.sql
@@ -0,0 +1,41 @@
+CREATE FUNCTION twitch.stream_settings_get(
+    channel_ VARCHAR,
+    start_date_ TIMESTAMP WITH TIME zone
+)
+    RETURNS TABLE
+            (
+                channel             VARCHAR,
+                start_date          TIMESTAMP without TIME zone,
+                thanks              BIT,
+                announce_milestones BIT,
+                chat_welcomes       BIT,
+                raid_welcomes       BIT,
+                shout_outs          BIT
+            )
+    LANGUAGE plpgsql
+AS
+$$
+
+BEGIN
+    RETURN QUERY (SELECT s.channel,
+                         s.start_date,
+                         s.thanks,
+                         s.announce_milestones,
+                         s.chat_welcomes,
+                         s.raid_welcomes,
+                         s.shout_outs
+
+                  FROM twitch.stream_settings s
+                  WHERE s.channel = channel_
+                    AND s.start_date = start_date_);
+END;
+$$;
+
+ALTER FUNCTION twitch.stream_settings_get (
+    VARCHAR,
+    TIMESTAMP WITH TIME zone
+    ) OWNER TO markr;
+
+GRANT EXECUTE
+    ON FUNCTION twitch.stream_settings_get(VARCHAR, TIMESTAMP WITH TIME zone)
+    TO notificationbot;
