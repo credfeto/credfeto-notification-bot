867955d9f048ab2f15934924ac573886abccb193
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-08-18 23:21:16 +0100

    removed

--- postgres-db/twitch/online_channel.sql
+++ postgres-db/twitch/online_channel.sql
@@ -3,7 +3,8 @@ CREATE TABLE twitch.online_channel
     username     VARCHAR(100) NOT NULL
         CONSTRAINT online_channel_pk PRIMARY KEY,
     date_created TIMESTAMP    NOT NULL,
-    id           VARCHAR(100) NOT NULL
+
+    enabled boolean NOT NULL
 );
 
 ALTER TABLE twitch.online_channel
