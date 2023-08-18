c5be6882a1d4bf5f033912569501c06d02e757cc
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-08-18 23:25:40 +0100

    online_channel_get_all

---
+++ postgres-db/twitch/online_channel_get_all.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION twitch.online_channel_get_all()
+    RETURNS TABLE
+            (
+                username     VARCHAR,
+                date_created TIMESTAMP without TIME zone
+            )
+    LANGUAGE plpgsql
+AS
+$$
+
+BEGIN
+    RETURN QUERY (SELECT s.username, s.date_created FROM twitch.online_channel s);
+END;
+$$;
+
+ALTER FUNCTION twitch.online_channel_get_all () OWNER TO markr;
+
+GRANT EXECUTE ON FUNCTION twitch.online_channel_get_all () TO notificationbot;
