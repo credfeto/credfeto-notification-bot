f9f7da224ef5dd7ac96a98e96327756bffb20aee
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-08-18 15:21:50 +0100

    Source generation

---
+++ src/Credfeto.Notification.Bot.Database/Twitch/ObjectMappers/TwitchStreamerObjectMapper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using Credfeto.Database.Interfaces;
+using Credfeto.Notification.Bot.Database.Twitch.Mappers;
+using Credfeto.Notification.Bot.Twitch.DataTypes;
+
+namespace Credfeto.Notification.Bot.Database.Twitch.ObjectMappers;
+
+internal static partial class TwitchStreamerObjectMapper
+{
+    [SqlObjectMap(name: "twitch.streamer_insert", sqlObjectType: SqlObjectType.STORED_PROCEDURE)]
+    public static partial ValueTask StreamerInsertAsync(DbConnection connection,
+                                                        [SqlFieldMap<StreamerMapper, Streamer>] Streamer channel,
+                                                        int id,
+                                                        [SqlFieldMap<DateTimeOffsetMapper, DateTimeOffset>] DateTimeOffset start_date,
+                                                        CancellationToken cancellationToken);
+
+    [SqlObjectMap(name: "twitch.streamer_get", sqlObjectType: SqlObjectType.STORED_PROCEDURE)]
+    public static partial ValueTask<TwitchUser?> StreamerGetAsync(DbConnection connection, [SqlFieldMap<StreamerMapper, Streamer>] Streamer channel, CancellationToken cancellationToken);
+
+    [SqlObjectMap(name: "twitch.streamer_get", sqlObjectType: SqlObjectType.STORED_PROCEDURE)]
+    public static partial ValueTask<TwitchUser?> StreamerGetAsync(DbConnection connection, [SqlFieldMap<ViewerMapper, Viewer>] Viewer channel, CancellationToken cancellationToken);
+}
