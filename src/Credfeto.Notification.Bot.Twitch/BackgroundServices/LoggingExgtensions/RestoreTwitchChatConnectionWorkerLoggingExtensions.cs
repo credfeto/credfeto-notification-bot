f200d5f6b9de08865267eb28ea65d7f2bde05f0b
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-10-25 12:07:49 +0100

    Logging extension methods

---
+++ src/Credfeto.Notification.Bot.Twitch/BackgroundServices/LoggingExgtensions/RestoreTwitchChatConnectionWorkerLoggingExtensions.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Credfeto.Notification.Bot.Twitch.BackgroundServices.LoggingExgtensions;
+
+internal static partial class RestoreTwitchChatConnectionWorkerLoggingExtensions
+{
+    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "Failed to update twitch chat connection: {message}")]
+    public static partial void FailedToUpdateTwitchChatConnection(this ILogger<RestoreTwitchChatConnectionWorker> logger, string message, Exception exception);
+}
