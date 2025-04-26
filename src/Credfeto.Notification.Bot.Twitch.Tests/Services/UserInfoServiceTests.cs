828ffd5476098225612a4e12333b5184624b760f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 12:27:12 +0000

    Cleanup: UserInfoServiceTests.cs

--- src/Credfeto.Notification.Bot.Twitch.Tests/Services/UserInfoServiceTests.cs
+++ src/Credfeto.Notification.Bot.Twitch.Tests/Services/UserInfoServiceTests.cs
@@ -19,14 +19,9 @@ public sealed class UserInfoServiceTests : TestBase
     public UserInfoServiceTests()
     {
         IOptions<TwitchBotOptions> options = GetSubstitute<IOptions<TwitchBotOptions>>();
-        options.Value.Returns(
-            new TwitchBotOptions(authentication: MockReferenceData.TwitchAuthentication, [])
-        );
+        options.Value.Returns(new TwitchBotOptions(authentication: MockReferenceData.TwitchAuthentication, []));
 
-        this._userInfoService = new UserInfoService(
-            options: options,
-            this.GetTypedLogger<UserInfoService>()
-        );
+        this._userInfoService = new UserInfoService(options: options, this.GetTypedLogger<UserInfoService>());
     }
 
     [Fact]
