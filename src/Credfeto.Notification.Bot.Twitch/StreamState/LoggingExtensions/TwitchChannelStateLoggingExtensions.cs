7a8e99b764be61668e4a2b18d3e4d2f7334cdb10
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 13:20:40 +0000

    Cleanup: TwitchChannelStateLoggingExtensions.cs

--- src/Credfeto.Notification.Bot.Twitch/StreamState/LoggingExtensions/TwitchChannelStateLoggingExtensions.cs
+++ src/Credfeto.Notification.Bot.Twitch/StreamState/LoggingExtensions/TwitchChannelStateLoggingExtensions.cs
@@ -5,17 +5,9 @@ namespace Credfeto.Notification.Bot.Twitch.StreamState.LoggingExtensions;
 
 internal static partial class TwitchChannelStateLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 1,
-        Level = LogLevel.Information,
-        Message = "{streamer}: Going online..."
-    )]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "{streamer}: Going online...")]
     public static partial void ChannelGoingOnline(this ILogger logger, Streamer streamer);
 
-    [LoggerMessage(
-        EventId = 2,
-        Level = LogLevel.Information,
-        Message = "{streamer}: Going offline..."
-    )]
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "{streamer}: Going offline...")]
     public static partial void ChannelGoingOffline(this ILogger logger, Streamer streamer);
 }
