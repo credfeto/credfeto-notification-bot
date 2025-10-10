98a90c1a3c53b7069fbd19df0a3ec6034f9f064b
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-10 17:26:09 +0000

    Cleanup: MockTwitchAuthentication.cs

--- src/Credfeto.Notification.Bot.Mocks/Objects/MockTwitchAuthentication.cs
+++ src/Credfeto.Notification.Bot.Mocks/Objects/MockTwitchAuthentication.cs
@@ -1,4 +1,4 @@
-﻿using Credfeto.Notification.Bot.Twitch.Configuration;
+using Credfeto.Notification.Bot.Twitch.Configuration;
 using FunFair.Test.Common.Mocks;
 
 namespace Credfeto.Notification.Bot.Mocks.Objects;
@@ -6,15 +6,18 @@ namespace Credfeto.Notification.Bot.Mocks.Objects;
 internal sealed class MockTwitchAuthentication : MockBase<TwitchAuthentication>
 {
     private static readonly TwitchAuthentication Invalid = new()
-                                                           {
-                                                               Api = new() { ClientId = "INVALID", ClientSecret = "INVALID", ClientAccessToken = "INVALID" },
-                                                               Chat = new() { OAuthToken = "INVALID", UserName = "INVALID" }
-                                                           };
+    {
+        Api = new()
+        {
+            ClientId = "INVALID",
+            ClientSecret = "INVALID",
+            ClientAccessToken = "INVALID",
+        },
+        Chat = new() { OAuthToken = "INVALID", UserName = "INVALID" },
+    };
 
     public MockTwitchAuthentication()
-        : base(Invalid)
-    {
-    }
+        : base(Invalid) { }
 
     public override TwitchAuthentication Next()
     {
