e44e7771fec1e543c24febe4def789f8fe47b2de
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-13 23:16:10 +0000

    Code generated ascii banner

--- src/Credfeto.Notification.Bot.Server/Helpers/StartupBanner.cs
+++ src/Credfeto.Notification.Bot.Server/Helpers/StartupBanner.cs
@@ -1,23 +1,17 @@
 using System;
+using Figgle;
 
 namespace Credfeto.Notification.Bot.Server.Helpers;
 
-internal static class StartupBanner
+// https://www.figlet.org/examples.html
+[GenerateFiggleText(memberName: "Banner", fontName: "basic", sourceText: "Mod-Bot")]
+internal static partial class StartupBanner
 {
     public static void Show()
     {
-        const string banner =
-            @"
-.88b  d88.  .d88b.  d8888b.        d8888b.  .d88b.  d888888b
-88'YbdP`88 .8P  Y8. 88  `8D        88  `8D .8P  Y8. `~~88~~'
-88  88  88 88    88 88   88        88oooY' 88    88    88
-88  88  88 88    88 88   88 C8888D 88~~~b. 88    88    88
-88  88  88 `8b  d8' 88  .8D        88   8D `8b  d8'    88
-YP  YP  YP  `Y88P'  Y8888D'        Y8888P'  `Y88P'     YP";
-        Console.WriteLine(banner);
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("");
+        Console.WriteLine(Banner);
+        Console.WriteLine();
+        Console.WriteLine();
         Console.WriteLine($"{VersionInformation.Product} ({VersionInformation.Version}): Starting...");
         Console.WriteLine("");
     }
